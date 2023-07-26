b82a804ac42a97ef927aca4e64d12fa78c18f3f3
Victor Smolyakov <smolyakov.92@bk.ru>
2023-07-27 01:02:19 +0500

    Created script for headphone profile changing by shortcut

---
+++ switch_headphone_profile.js
@@ -0,0 +1,35 @@
+#!/bin/env node
+
+const {execSync} = require('child_process');
+const getValue = require('./utils').getValue;
+const modes = {
+	'a2dp_sink': 'a2dp_sink', 'headset_head_unit': 'headset_head_unit', 'off': 'off',
+};
+const commands = {
+	'get_headphone_profile': "pactl list cards | awk -v RS='' '/bluez/' | awk -F': ' '/Active Profile/ { print $2 }'",
+	'set_headphone_profile': 'pactl set-card-profile bluez_card.F8_AB_E5_65_5B_67'
+};
+
+try {
+	const deviceName = getValue('--device_name');
+	const currentProfile = execSync(commands.get_headphone_profile, {encoding: 'utf8'}).trim();
+
+	console.info(`The current headphone profile is ${currentProfile}`);
+
+	switch (currentProfile) {
+		case modes.a2dp_sink:
+			execSync(`${commands.set_headphone_profile} ${modes.headset_head_unit}`);
+			console.info(`The headphone has been switched to the ${modes.headset_head_unit} profile`);
+			break;
+		case modes.headset_head_unit:
+		case modes.off:
+		default:
+			execSync(`${commands.set_headphone_profile} ${modes.a2dp_sink}`);
+			console.info(`The headphone has been switched to the ${modes.a2dp_sink} profile`);
+			break;
+	}
+} catch (error) {
+	console.error(`Error: ${error.message} | ${error}`);
+}
+
+process.exit(0);
