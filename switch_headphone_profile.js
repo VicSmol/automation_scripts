4ef44fd17daa6d3e41cd5cf613ee81640e052b07
Victor Smolyakov <smolyakov.92@bk.ru>
2023-07-27 20:39:48 +0500

    Refactor script

--- switch_headphone_profile.js
+++ switch_headphone_profile.js
@@ -4,33 +4,22 @@
 
 const {execSync} = require('child_process');
 const getValue = require('./utils').getValue;
+const deviceName = getValue('--device_name');
 const modes = {
 	'a2dp_sink': 'a2dp_sink', 'headset_head_unit': 'headset_head_unit', 'off': 'off',
 };
 const commands = {
-	'get_headphone_profile': "pactl list cards | awk -v RS='' '/bluez/' | awk -F': ' '/Active Profile/ { print $2 }'",
-	'set_headphone_profile': 'pactl set-card-profile bluez_card.F8_AB_E5_65_5B_67'
+	'get_headphone_profile': `pactl list cards | awk -v RS='' '/${deviceName}/' | awk -F': ' '/Active Profile/ { print $2 }'`,
+	'set_headphone_profile': `pactl set-card-profile ${deviceName}`
 };
 
 try {
-	// TODO Fix --device_name arg parsing
-	const deviceName = getValue('--device_name');
 	const currentProfile = execSync(commands.get_headphone_profile, {encoding: 'utf8'}).trim();
+	const targetProfile = currentProfile === modes.a2dp_sink ? modes.headset_head_unit : modes.a2dp_sink;
 
 	console.info(`The current headphone profile is ${currentProfile}`);
-
-	switch (currentProfile) {
-		case modes.a2dp_sink:
-			execSync(`${commands.set_headphone_profile} ${modes.headset_head_unit}`);
-			console.info(`The headphone has been switched to the ${modes.headset_head_unit} profile`);
-			break;
-		case modes.headset_head_unit:
-		case modes.off:
-		default:
-			execSync(`${commands.set_headphone_profile} ${modes.a2dp_sink}`);
-			console.info(`The headphone has been switched to the ${modes.a2dp_sink} profile`);
-			break;
-	}
+	execSync(`${commands.set_headphone_profile} ${targetProfile}`);
+	console.info(`The headphone has been switched to the ${targetProfile} profile`);
 } catch (error) {
 	console.error(`Error: ${error.message} | ${error}`);
 	process.exit(1);
