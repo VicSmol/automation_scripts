f2e87bf665dfffe97ec550423bdf940d0eeee027
Victor Smolyakov <smolyakov.92@bk.ru>
2023-08-03 00:31:09 +0500

    Finished updateVirtualbox.js script

--- updateVirtualbox.js
+++ updateVirtualbox.js
@@ -1,22 +1,44 @@
-"use strict";
+#!/bin/env node
 
-const {execSync} = require("node:child_process");
-const commands = [
-	'apt update',
-	'apt install --reinstall linux-headers-$(uname -r) virtualbox-dkms dkms',
-	'modprobe vboxdrv',
-	// 'reboot': 'reboot',
+//TODO There's need to write description about this script
+
+'use strict';
+
+const {spawnSync} = require("node:child_process");
+const instructions = [
+	{
+		command: 'apt', args: ['update']
+	},
+	{
+		command: 'apt', args: [
+			'install',
+			'--reinstall',
+			'linux-headers-$(uname -r)',
+			'virtualbox-dkms',
+			'dkms'
+		]
+	},
+	{
+		command: 'modprobe', args: ['vboxdrv']
+	}
 ];
 
-console.info('VirtualBox update start');
+console.info('VirtualBox update start\n');
 
 try {
-	commands.forEach(command => {
-		console.info(`The command "${command}" is start execute`);
-		execSync(command);
-		console.info(`The command ${command} has been executed`);
+	instructions.forEach((instruction, index) => {
+		console.log(`---------Command ${index + 1}-----------`);
+		console.info(`The command "${instruction.command} ${instruction.args.join(' ')}" is start execute`);
+
+		const {stdout, stderr, status, signal, pid} = spawnSync(instruction.command, instruction.args, {encoding: 'utf8'});
+
+		console.debug({pid, status, signal, stdout, stderr});
+		console.info(`The command "${instruction.command} ${instruction.args.join(' ')}" has been executed`);
+		console.log(`-----------------------------------------\n`);
 	});
 } catch (error) {
 	console.error(`ERROR: ${error}`);
 	process.exit(1);
 }
+
+console.info('VirtualBox has been updated\n');
