afbeb1b5e19d7d0189c0e1a3d9b1ab825d902598
Victor Smolyakov <smolyakov.92@bk.ru>
2023-08-07 01:40:32 +0500

    Fixed makeSshTunnels.js script

--- makeSshTunnels.js
+++ makeSshTunnels.js
@@ -5,11 +5,12 @@
 'use strict';
 
 const fs = require('node:fs');
-const {execFile, execFileSync, execSync} = require('node:child_process');
+const {spawn, execSync} = require('node:child_process');
 const extractValue = require('./utils').extractValue;
+const isShutdown = extractValue('--shutdown') === 'true';
 const remoteUser = extractValue('--remote-user');
 const host = extractValue('--host');
-const port = extractValue('--port');
+const port = parseInt(extractValue('--port'));
 const privateKeyPath = extractValue('--private-key-path');
 const datasetPath = extractValue('--dataset');
 //TODO ask DEVOPS about every service and write description for it
@@ -47,7 +48,7 @@ try {
 	console.info('          Start hosts file backup checking...');
 
 	if (fs.existsSync(backupHostsPath)) {
-		console.info('          Hosts backup file file exist |+|');
+		console.info('          Hosts backup file exists |+|');
 	} else {
 		console.info('          Start creating hosts backup file...');
 
@@ -59,20 +60,20 @@ try {
 	console.info('          Hosts backup checked! |+|');
 	console.info('---------------------------------------------------------------\n');
 
-	console.info('------------------2) Loopback networks cleaning----------------');
-	console.info('          Start additional Loopback networks cleaning...');
-
-	removeLoopbackNetworks();
-
-	console.info('          Loopback networks cleaning was finished |+|');
-	console.info('---------------------------------------------------------------\n');
-
-	console.info('------------------3) Copy hosts backup file to hosts-----------');
+	console.info('------------------2) Copy hosts backup file to hosts-----------');
 	console.info('          Start copy backup of hosts file to hosts...');
 
 	fs.cpSync(backupHostsPath, hostsPath);
 
 	console.info('          Backup of hosts file to hosts was finished |+|');
+	console.info('---------------------------------------------------------------\n');
+
+	console.info('------------------3) Loopback networks cleaning----------------');
+	console.info('          Start additional Loopback networks cleaning...');
+
+	removeLoopbackNetworks();
+
+	console.info('          Loopback networks cleaning was finished |+|');
 	console.info('---------------------------------------------------------------\n');
 
 	console.info('------------------4) Old ssh tunnels cleaning------------------');
@@ -83,20 +84,24 @@ try {
 	console.info('          Old ssh tunnels cleaning was finished |+|');
 	console.info('---------------------------------------------------------------\n');
 
+	if (isShutdown) {
+		process.exit(0);
+	}
+
 	console.info('------------------5) Appending networks to loopback interface--');
-	console.info('          Start old ssh tunnels cleaning...');
+	console.info('          Start appending networks to loopback interface...');
 
-	dataset.forEach(service => execFileSync(`ip addr add ${service.network}/8 dev lo`));
+	dataset.forEach(service => execSync(`ip addr add ${service.network}/8 dev lo`, {encoding: 'utf-8'}));
 
-	console.info('          Old ssh tunnels cleaning was finished |+|');
+	console.info('          Appending networks to loopback interface was finished |+|');
 	console.info('---------------------------------------------------------------\n');
 
-	console.info('------------------6) Forwarded services append to hosts file---');
+	console.info('------------------6) Append forwarded services to hosts file---');
 	console.info('          Start appending services to hosts file...');
 
 	dataset.forEach(service => {
 		service.hosts.forEach(host => {
-			fs.appendFileSync(hostsPath, `${service.network} ${host}`);
+			fs.appendFileSync(hostsPath, `${service.network} ${host}\n`, {encoding: 'utf-8', flag: 'a+'});
 		});
 	});
 
@@ -107,16 +112,33 @@ try {
 	console.info('          Start SSH tunnels creating...');
 
 	dataset.forEach(service =>
-		service.forwarding.forEach(tunnel =>
-			execFile("ssh -N -n -i privateKeyPath -o ExitOnForwardFailure=yes -o StrictHostKeyChecking=no -L " +
-				`${tunnel} ${remoteUser}@${host} -p	${port}`))
-	);
+		service.forwarding.forEach(tunnel => {
+			const instruction = (`ssh -N -n -i ${privateKeyPath} -o ExitOnForwardFailure=yes -o StrictHostKeyChecking=no -L ` +
+				`${tunnel} ${remoteUser}@${host} -p ${port}`)
+				.split(' ')
+				.filter(item => item).reduce((acc, item, index) => {
+					if (index === 0) {
+						acc['command'] = item;
+						acc['args'] = [];
+					} else {
+						acc['args'].push(item);
+					}
+
+					return acc;
+				}, {});
+			const ssh = spawn(instruction.command, instruction.args, {detached: true});
+
+			ssh.on('error', (err) => {
+				console.error(err);
+			});
+			ssh.unref();
+		}));
 
 	console.info('          SSH tunnels creating was finished |+|');
 	console.info('---------------------------------------------------------------\n');
+
+	process.exit(0);
 } catch (error) {
 	console.error(`Error: ${error.message} | ${error}`);
 	process.exit(1);
 }
-
-process.exit(0);
