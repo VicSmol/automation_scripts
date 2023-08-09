5aec55feac81ba917b999373c0a1d4628d0f6b03
Victor Smolyakov <smolyakov.92@bk.ru>
2023-08-10 00:37:15 +0500

    Updated script switchOutputDevices.js

--- switchOutputDevices.js
+++ switchOutputDevices.js
@@ -7,31 +7,23 @@
 const {execSync} = require('node:child_process');
 const extractValue = require('./utils').extractValue;
 const isReverseOrder = extractValue('--reverse-order') === true;
-const getOutputDevices = () =>
-	execSync("pacmd list-sinks " +
-		"| grep 'name: ' " +
-		"| grep -E '[^<>]+' " +
-		"| awk '{print $2}'", {encoding: 'utf-8'})
-		.split('\n')
-		.filter(deviceName => deviceName)
-		.map(deviceName => deviceName.slice(1, -1));
-const getActiveDevice = () =>
-	execSync("pactl info " +
-		"| grep -iE 'Default\\s+Sink' " +
-		"| awk '{print $3}'", {encoding: 'utf-8'})
-		.trim();
+const getOutputDevices = () => execSync("pacmd list-sinks " + "| grep 'name: ' " + "| grep -E '[^<>]+' " + "| awk '{print $2}'", {encoding: 'utf-8'})
+	.split('\n')
+	.filter(deviceName => deviceName)
+	.map(deviceName => deviceName.slice(1, -1));
+const getActiveDevice = () => execSync("pactl info " + "| grep -iE 'Default\\s+Sink' " + "| awk '{print $3}'", {encoding: 'utf-8'})
+	.trim();
 /**
  * @param {string[]} devices
  * @param {string} currentDevice
  */
-const switchOutputDevice = (devices, currentDevice, flag = true) => {
+const switchOutputDevice = (devices, currentDevice, isReverseOrder = false) => {
 	const index = devices.findIndex((value) => value === currentDevice);
-	const diff = flag ? 1 : -1;
+	const diff = isReverseOrder ? -1 : 1;
 	const nextIndex = (index + diff + devices.length) % devices.length;
+	const cliOutput = execSync(`pacmd set-default-sink ${devices[nextIndex]}`, {encoding: 'utf-8'});
 
-	const temp = execSync(`pacmd set-default-sink ${devices[nextIndex]}`, {encoding: 'utf-8'});
-
-	console.log(temp);
+	console.debug(cliOutput);
 }
 
 try {
