8f9970e9572b36f27a4899d835908945f67d2a99
Victor Smolyakov <smolyakov.92@bk.ru>
2023-08-10 00:34:53 +0500

    Created script MVP (output audio device switcher)

---
+++ switchOutputDevices.js
@@ -0,0 +1,47 @@
+#!/bin/env node
+
+//TODO There's need to write description about this script
+
+'use strict';
+
+const {execSync} = require('node:child_process');
+const extractValue = require('./utils').extractValue;
+const isReverseOrder = extractValue('--reverse-order') === true;
+const getOutputDevices = () =>
+	execSync("pacmd list-sinks " +
+		"| grep 'name: ' " +
+		"| grep -E '[^<>]+' " +
+		"| awk '{print $2}'", {encoding: 'utf-8'})
+		.split('\n')
+		.filter(deviceName => deviceName)
+		.map(deviceName => deviceName.slice(1, -1));
+const getActiveDevice = () =>
+	execSync("pactl info " +
+		"| grep -iE 'Default\\s+Sink' " +
+		"| awk '{print $3}'", {encoding: 'utf-8'})
+		.trim();
+/**
+ * @param {string[]} devices
+ * @param {string} currentDevice
+ */
+const switchOutputDevice = (devices, currentDevice, flag = true) => {
+	const index = devices.findIndex((value) => value === currentDevice);
+	const diff = flag ? 1 : -1;
+	const nextIndex = (index + diff + devices.length) % devices.length;
+
+	const temp = execSync(`pacmd set-default-sink ${devices[nextIndex]}`, {encoding: 'utf-8'});
+
+	console.log(temp);
+}
+
+try {
+	const outputDevices = getOutputDevices();
+	const activeDevice = getActiveDevice();
+
+	switchOutputDevice(outputDevices, activeDevice, isReverseOrder);
+} catch (error) {
+	console.error(`Error: ${error.message} | ${error}`);
+	process.exit(1);
+}
+
+process.exit(0);
