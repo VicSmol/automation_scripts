6b91b5073a82217b59656540d3035e57c6f545ec
Victor Smolyakov <smolyakov.92@bk.ru>
2023-08-24 01:18:12 +0500

    Fixed updateVirtualbox.js script

--- updateVirtualbox.js
+++ updateVirtualbox.js
@@ -4,46 +4,51 @@
 
 'use strict';
 
-const {spawnSync} = require("node:child_process");
-const instructions = [
-	{
-		command: 'apt', args: ['update']
-	},
-	{
-		command: 'apt', args: [
-			'install',
-			'-y',
-			'--reinstall',
-			'linux-headers-$(uname -r)',
-			'virtualbox-dkms',
-			'dkms'
-		]
-	},
-	{
-		command: 'modprobe', args: ['vboxdrv']
-	}
-];
+const {execFileSync, execSync} = require("node:child_process");
 
 console.info('VirtualBox update start\n');
 
 try {
+	const linuxKernelVersion = execSync('uname -r', {encoding: 'utf-8'}).trim();
+	const instructions = [
+		{
+			command: 'apt-get', args: ['update']
+		},
+		{
+			command: 'apt-get', args: [
+				'install',
+				'-y',
+				'--reinstall',
+				`linux-headers-${linuxKernelVersion}`,
+				'virtualbox-dkms',
+				'dkms'
+			]
+		},
+		{
+			command: 'modprobe', args: ['vboxdrv']
+		}
+	];
+
+	console.debug(`The Linux kernel version is ${linuxKernelVersion}\n`);
+
 	instructions.forEach((instruction, index) => {
-		console.log(`---------Command ${index + 1}-----------`);
+		console.log(`--------------Command ${index + 1}-----------------`);
 		console.info(`The command "${instruction.command} ${instruction.args.join(' ')}" is start execute\n`);
 
-		const {stdout, stderr, status, signal, pid} = spawnSync(instruction.command, instruction.args, {encoding: 'utf8'});
+		const output = execFileSync(instruction.command, instruction.args, {encoding: 'utf-8'});
 
-		console.debug({pid, status, signal, stdout, stderr});
+		console.debug(output);
 		console.info(`\nThe command "${instruction.command} ${instruction.args.join(' ')}" has been executed`);
-		console.log(`-----------------------------------------\n`);
+		console.log(`----------------------------------------\n`);
 	});
 
 	console.info('VirtualBox has been updated\n');
+
+	process.exit(0);
 } catch (error) {
-	console.info('VirtualBox has not been updated\n');
+	console.error('VirtualBox has not been updated!\n');
 	console.error(`ERROR: ${error}`);
+	console.error(error);
 
 	process.exit(1);
 }
-
-process.exit(0);
