d39360ea585ea853d25dee4592f5289230fba877
Victor Smolyakov <smolyakov.92@bk.ru>
2024-05-09 20:58:49 +0500

    Refactored script

--- removeDirectories.js
+++ removeDirectories.js
@@ -1,3 +1,5 @@
+#!/bin/env node
+
 //TODO There's need to write description about this script
 
 'use strict';
@@ -8,10 +10,14 @@ const extractValue = require('./utils.js').extractValue;
 const startDirectory = extractValue('--start-path');
 const removableDirectory = extractValue('--remove-dir');
 
-fs.readdir(startDirectory, {withFileTypes: true}).then(dirents => dirents.filter(entity => entity.isDirectory())
-	.map(dirent => path.join(startDirectory, dirent.name, removableDirectory))
-	.map((dirName, index) => fs.access(dirName)
-		.then(() => fs.rm(dirName, {recursive: true, force: true})
-			.then(() => console.info(`INFO: (${index}) Directory ${dirName} was removed`))
-			.catch(error => console.error(`ERROR: (${index}) Directory ${dirName} wasn't removed | ${error.message}`)))
-		.catch(error => console.error(`ERROR: (${index}) Directory ${dirName} wasn't removed | ${error.message}`))));
+fs.readdir(startDirectory, {withFileTypes: true})
+	.then(dirents => {
+		const directories = dirents.filter(entity => entity.isDirectory());
+		return Promise.all(directories.map(dirent => {
+			const dirName = path.join(startDirectory, dirent.name, removableDirectory);
+			return fs.rm(dirName, {recursive: true, force: true})
+				.then(() => console.info(`INFO: Directory ${dirName} was removed`))
+				.catch(error => console.error(`ERROR: Directory ${dirName} wasn't removed | ${error.message}`));
+		}));
+	})
+	.catch(error => console.error(`Error while reading directory: ${error.message}`));
